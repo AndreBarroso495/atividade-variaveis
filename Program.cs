3f977ce6dabb997eede6d67376ab90028ce1fce1
FIC <FIC@info.local>
2020-11-18 10:40:56 -0300

    Projeto concluido

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace atividade_funcaowhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int nota;
+             do
+             {
+                 Console.WriteLine("Digite uma nota entre 0 e 10");
+                 nota = int.Parse(Console.ReadLine());
+                 
+             } while (nota < 0 || nota>10);
+             Console.WriteLine("Obrigado");
+
+        }
+    }
+}
